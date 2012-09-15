eef3e9c607af40fec27d208e96d78d2e8fe7b22f
Merouane Atig <yahoocbien-dev@yahoo.fr>
2012-09-15 23:59:24 +0200

    create first models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.model):
+    name = models.CharField(max_length=200)
+
+class Post(models.model):
+    blog = models.ForeignKey(Blog)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_at = models.DateTimeField()
