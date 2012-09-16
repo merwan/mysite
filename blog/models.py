8950de39cc9a439cb11f9d83219ea2085ed4428c
Merouane Atig <yahoocbien-dev@yahoo.fr>
2012-09-16 22:15:39 +0200

    define basic methods

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,21 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 class Blog(models.Model):
     name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.name
 
 class Post(models.Model):
     blog = models.ForeignKey(Blog)
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_at = models.DateTimeField()
+
+    def __unicode__(self):
+        return self.title
+
+    def was_published_recently(self):
+        return self.published_at >= timezone.now() - datetime.timedelta(days=1)
